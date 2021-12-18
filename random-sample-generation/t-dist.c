fabdbba7e0e4ad3f531a2df0d51b8b006c2986c1
Rishav-12 <rishavmitra10@gmail.com>
2021-12-18 14:01:05 +0000

    :sparkles: more sample generation

---
+++ random-sample-generation/t-dist.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<time.h>
+int main()
+{
+	int i,j,n,m;
+	float r1,r2,x,y,z;
+	printf("Enter the d.f. of the t distribution:\n");
+	scanf("%d",&m);
+	printf("Enter the number of observations to be generated:\n");
+	scanf("%d",&n);
+	srand(time(0));
+	printf("The generated sample is:\n");
+	for(i=1;i<=n;i++)
+	{
+		y=0;
+		for(j=1;j<=m;j++)
+		{
+			r1=rand()/(float)RAND_MAX;
+			r2=rand()/(float)RAND_MAX;
+			x=sqrt(-2*log(r1))*sin(2*3.14*r2);
+			y=y+x*x;
+		}
+		x=sqrt(-2*log(r1))*cos(2*3.14*r2);
+		z = x/sqrt(y/m);
+		printf("%f\n",z);
+	}
+}
